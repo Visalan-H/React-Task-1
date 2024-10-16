93db19e26b74ba08933db331b57ce7defe7d35e9
Visalan <visalanoffl@gmail.com>
2024-10-16 21:03:53 +0530

    minor changes

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -6,10 +6,10 @@ import Categories from '../Categories/Categories.jsx'
 export default function Portfolio() {
 
     const galleryItems = [
-        { category: 'Illustrations', imgs: ['https://via.placeholder.com/250/ff5733/ffffff?text=Illustrations', 'https://via.placeholder.com/250/ff5733/ffffff?text=Illustrations', 'https://via.placeholder.com/250/ff5733/ffffff?text=Illustrations', 'https://via.placeholder.com/250/ff5733/ffffff?text=Illustrations'] },
-        { category: 'Photography', imgs: ['https://via.placeholder.com/250/33c1ff/ffffff?text=Photography', 'https://via.placeholder.com/250/33c1ff/ffffff?text=Photography', 'https://via.placeholder.com/250/33c1ff/ffffff?text=Photography', 'https://via.placeholder.com/250/33c1ff/ffffff?text=Photography'] },
-        { category: 'Websites', imgs: ['https://via.placeholder.com/250/00cc44/ffffff?text=Websites', 'https://via.placeholder.com/250/00cc44/ffffff?text=Websites', 'https://via.placeholder.com/250/00cc44/ffffff?text=Websites', 'https://via.placeholder.com/250/00cc44/ffffff?text=Websites'] },
-        { category: 'Art', imgs: ['https://via.placeholder.com/250/9933ff/ffffff?text=Art', 'https://via.placeholder.com/250/9933ff/ffffff?text=Art', 'https://via.placeholder.com/250/9933ff/ffffff?text=Art', 'https://via.placeholder.com/250/9933ff/ffffff?text=Art'] }
+        { category: 'Illustrations', imgs: ['https://via.placeholder.com/250/ff5733/ffffff?text=Illustration1', 'https://via.placeholder.com/250/ff5733/ffffff?text=Illustration2', 'https://via.placeholder.com/250/ff5733/ffffff?text=Illustration3', 'https://via.placeholder.com/250/ff5733/ffffff?text=Illustration4'] },
+        { category: 'Photography', imgs: ['https://via.placeholder.com/250/33c1ff/ffffff?text=Photo1', 'https://via.placeholder.com/250/33c1ff/ffffff?text=Photo2', 'https://via.placeholder.com/250/33c1ff/ffffff?text=Photo3', 'https://via.placeholder.com/250/33c1ff/ffffff?text=Photo4'] },
+        { category: 'Websites', imgs: ['https://via.placeholder.com/250/00cc44/ffffff?text=Website1', 'https://via.placeholder.com/250/00cc44/ffffff?text=Website2', 'https://via.placeholder.com/250/00cc44/ffffff?text=Website3', 'https://via.placeholder.com/250/00cc44/ffffff?text=Website4'] },
+        { category: 'Art', imgs: ['https://via.placeholder.com/250/9933ff/ffffff?text=Art1', 'https://via.placeholder.com/250/9933ff/ffffff?text=Art2', 'https://via.placeholder.com/250/9933ff/ffffff?text=Art3', 'https://via.placeholder.com/250/9933ff/ffffff?text=Art4'] }
     ];
 
 
